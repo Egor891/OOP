fe14edf942a13d772ca727f9047de175368380fc
Egor <nanana77789@gmail.com>
2024-11-16 14:14:47 +0300

    new add_1

--- OOP.py
+++ OOP.py
@@ -14,6 +14,8 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,6 +24,10 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+
+class Lecturer(Mentor):
+    pass
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
